63d608797a72eefa6473b8f5b82e1ab960cd8aee
Alessandro Guedes <alessandro.guedess@gmail.com>
2023-05-24 14:36:18 -0300

    Add breack line from text

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,11 @@
 module ApplicationHelper
   include Pagy::Frontend
+
   def link_icon(url, css_class, icon, text)
     "<a href='#{url}' class='#{css_class}'><i class='#{icon}'></i> #{text} </a>".html_safe
+  end
+
+  def text_with_breackline(text)
+    text.gsub(/\n/, '<br />').html_safe
   end
 end
