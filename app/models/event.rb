734f3e50cdfe94d376734feccd43af66bcc2fc52
Alessandro Guedes <alessandro.guedess@gmail.com>
2023-06-20 15:12:12 -0300

    Remove name from events

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,5 +1,5 @@
 class Event < ApplicationRecord
-  validates :name, :date, presence: true
+  validates :date, presence: true
   belongs_to :hire
   belongs_to :project
   has_one :production_sheet, dependent: :destroy
