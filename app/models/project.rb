4ff37afc6e67d28da03f3059a42a65279bca7ab3
Alessandro Guedes <alessandro.guedess@gmail.com>
2023-07-04 11:53:04 -0300

    add logos in projects

--- app/models/project.rb
+++ app/models/project.rb
@@ -5,4 +5,8 @@ class Project < ApplicationRecord
   has_one_attached :office_logo
   has_one_attached :record_logo
   has_one_attached :extra_logo
+
+  def has_logo?
+    logo.attached? || office_logo.attached? || record_logo.attached? || extra_logo.attached?
+  end
 end
