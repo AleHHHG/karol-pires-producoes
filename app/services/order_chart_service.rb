baee2d5758ea0d51e842870c5cdf514b2872c63b
Alessandro Guedes <alessandro.guedess@gmail.com>
2023-07-10 17:23:32 -0300

    order graph labels by name

--- app/services/order_chart_service.rb
+++ app/services/order_chart_service.rb
@@ -9,12 +9,13 @@ class OrderChartService
   end
 
   def bar_chart_data
-    items = Order.by_period(initial_date, end_date).joins(:project).group('projects.name').count
+    items = Order.by_period(initial_date, end_date).joins(:project).group('projects.name').order('projects.name desc').count
     { labels: items.keys, values: items.values}
   end
 
   def pie_chart_data
-    items = Order.by_period(initial_date, end_date).select('projects.name as project_name, sum(price) as price').grouped_by_project
+    items = Order.by_period(initial_date, end_date).select('projects.name as project_name, sum(price) as price')
+                 .grouped_by_project.order('projects.name desc')
     { labels: items.map(&:project_name), values: items.map(&:price) }
   end
 end
